b2285c1a6b25084b97b2ba51e0940e74b678aa16
Domenico Nappo <domenico.nappo@gmail.com>
2019-05-13 17:47:37 +0200

    making setup safe when cython is not already installed

--- setup.py
+++ setup.py
@@ -1,16 +1,51 @@
 """
+Copyright 2019 European Union
+
+Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission  subsequent versions of the EUPL (the "Licence");
+
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+https://joinup.ec.europa.eu/sites/default/files/inline-files/EUPL%20v1_2%20EN(1).txt
+
+Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Licence for the specific language governing permissions and limitations under the Licence.
+
+---------------------------------------------------------------------------------------------------------------------------------------
+python setup.py sdist
+
+To upload new package on PyPi Test:
+twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
 To upload new package on PyPi:
 twine upload dist/*
+
+Test package install
+pip install --index-url https://test.pypi.org/simple/ lisflood-model==2.8.6
+
+In prod:
+pip install lisflood-model
 """
 
 import os
+import sys
 from glob import glob
 
-from Cython.Build import cythonize
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    def cythonize(*args, **kwargs):
+        from Cython.Build import cythonize
+        return cythonize(*args, **kwargs)
 
 from setuptools import setup, find_packages, Extension
 
 from lisflood.metainfo import __version__, __authors__
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(current_dir, './src/'))
+readme_file = os.path.join(current_dir, 'README.md')
 
 ext_modules = [
     Extension(
@@ -20,9 +55,6 @@ ext_modules = [
         extra_link_args=["-fopenmp"]
     )
 ]
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-readme_file = os.path.join(current_dir, 'readme.md')
 
 with open(readme_file, 'r') as f:
     long_description = f.read()
@@ -44,9 +76,18 @@ setup(
     license='EUPL 1.2',
     url='https://github.com/ec-jrc/lisflood-model',
     download_url='https://github.com/ec-jrc/lisflood-code/archive/{}.tar.gz'.format(__version__),
-    install_requires=['numpy>=1.15', 'pytest', 'netCDF4', 'python-dateutil', 'numexpr', 'cython', 'docopt', 'pandas', 'xarray', 'pyproj'],
+    setup_requires=[
+            # Setuptools 18.0 properly handles Cython extensions.
+            'setuptools>=18.0',
+            'cython',
+    ],
+    install_requires=[
+        'numpy>=1.15', 'pytest', 'netCDF4', 'python-dateutil',
+        'numexpr', 'cython', 'docopt', 'pandas', 'xarray', 'pyproj'
+    ],
     entry_points={'console_scripts': ['lisflood = lisflood.lisf1:main']},
-    ext_modules=cythonize(ext_modules, annotate=True),
+    # ext_modules=cythonize(ext_modules, annotate=True),
+    ext_modules=ext_modules,
     zip_safe=True,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
