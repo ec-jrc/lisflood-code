bd756fdd70285b5dcbbe98eb70a5d034080fb17e
Domenico Nappo <domenico.nappo@gmail.com>
2019-09-03 09:15:50 +0200

    implemented #6 on copernicus; changes will be in master after release of EFAS-6h #6

--- src/lisflood/hydrological_modules/routing.py
+++ src/lisflood/hydrological_modules/routing.py
@@ -349,7 +349,6 @@ class routing(object):
         # cumulative discharge at outlet [m3]
         self.var.TotalQInM3 = maskinfo.in_zero()
         # cumulative inflow from inflow hydrographs [m3]
-        #self.var.sumDis = maskinfo.in_zero()
         self.var.sumDis = maskinfo.in_zero()
         self.var.sumIn = maskinfo.in_zero()
 
@@ -388,7 +387,8 @@ class routing(object):
 
                 # TEMPORARY WORKAROUND FOR EFAS XDOM!!!!!!!!!!
                 # This must be removed
-                self.var.QLimit = self.var.QLimit / 24.0
+                # self.var.QLimit = self.var.QLimit / 24.0
+                # IMPLEMENTED #6 on copernicus branch
                 ###############################################
 
                 self.var.Chan2M3Start = self.var.ChannelAlpha2 * self.var.ChanLength * (self.var.QLimit ** self.var.Beta)
@@ -479,16 +479,16 @@ class routing(object):
                 #  ---- Single Routing ---------------
                 # No split routing
                 # side flow consists of runoff (incl. groundwater), inflow from reservoirs (optional) and external inflow hydrographs (optional)
-                SideflowChan[np.isnan(SideflowChan)] = 0 # TEMPORARY FIX - SEE DEBUG ABOVE!
+                SideflowChan[np.isnan(SideflowChan)] = 0  # TEMPORARY FIX - SEE DEBUG ABOVE!
 
                 # ChanQKin in [cu m / s]
                 self.river_router.kinematicWaveRouting(self.var.ChanQKin, SideflowChan, "main_channel")
                 self.var.ChanM3Kin = self.var.ChanLength * self.var.ChannelAlpha * self.var.ChanQKin**self.var.Beta
                 # Volume in channel at end of computation step
                 #self.var.ChanQKin=pcraster.max(self.var.ChanQKin,0)
-                self.var.ChanQ=np.maximum(self.var.ChanQKin,0)
+                self.var.ChanQ = np.maximum(self.var.ChanQKin, 0)
                 # at single kin. ChanQ is the same
-                self.var.sumDisDay+=self.var.ChanQ
+                self.var.sumDisDay += self.var.ChanQ
                 # Total channel storage [cu m], equal to ChanM3Kin
                 #self.var.ChanQ = maxpcr(self.var.ChanQKin, null)
 
@@ -500,11 +500,15 @@ class routing(object):
                 SideflowRatio=np.where((self.var.ChanM3Kin + self.var.Chan2M3Kin) > 0,self.var.ChanM3Kin/(self.var.ChanM3Kin+self.var.Chan2M3Kin),0.0)
 
                 # CM ##################################
-                self.var.Sideflow1Chan = np.where(self.var.ChanM3Kin > self.var.M3Limit, SideflowRatio*SideflowChan, SideflowChan)
+                # IMPLEMENTING #6 on copernicus branch
+                # self.var.Sideflow1Chan = np.where(self.var.ChanM3Kin > self.var.M3Limit, SideflowRatio*SideflowChan, SideflowChan)
                 # This is creating instability because ChanM3Kin can be < M3Limit between two routing sub-steps
                 #######################################
-                # self.var.Sideflow1Chan = np.where((self.var.ChanM3Kin + self.var.Chan2M3Kin-self.var.Chan2M3Start) > self.var.M3Limit,
-                #                                   SideflowRatio*SideflowChan, SideflowChan)
+                self.var.Sideflow1Chan = np.where(
+                    (self.var.ChanM3Kin + self.var.Chan2M3Kin-self.var.Chan2M3Start) > self.var.M3Limit,
+                    SideflowRatio*SideflowChan,
+                    SideflowChan
+                )
 
                 self.var.Sideflow1Chan = np.where(np.abs(SideflowChan) < 1e-7, SideflowChan, self.var.Sideflow1Chan)
                 # too small values are avoided
