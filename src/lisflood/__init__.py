b949145b49c4d6f74a6db57eb5d3d4feb68ea7a9
Domenico Nappo <domenico.nappo@gmail.com>
2020-02-04 18:32:10 +0100

    add dates vs steps tests #43

--- src/lisflood/__init__.py
+++ src/lisflood/__init__.py
@@ -1,7 +1,7 @@
 version = (2, 11, 1)
 __authors__ = "Ad de Roo, Emiliano Gelati, Peter Burek, Johan van der Knijff, Niko Wanders"
 __version__ = '.'.join(list(map(str, version)))
-__date__ = "23/01/2020"
+__date__ = "04/02/2020"
 __copyright__ = "Copyright 2020, European Commission - Joint Research Centre"
 __maintainer__ = "Emiliano Gelati, Domenico Nappo, Valerio Lorini, Lorenzo Mentaschi, Ad de Roo"
 __status__ = "Operation"
--- tests/test_dates_steps.py
+++ tests/test_dates_steps.py
@@ -14,12 +14,23 @@ settings_file_6hourly = os.path.join(os.path.dirname(__file__), 'data/settings/f
 
 class TestStepsDates(TestSettings):
 
+    def setup_method(self):
+        settings = self.setoptions(settings_file)
+        rm_files = [os.path.join(settings.output_dir, f) for f in os.listdir(settings.output_dir) if f.endswith('.nc') or f.endswith('.tss')]
+        for f in rm_files:
+            os.unlink(f)
+
+    def teardown_method(self):
+        settings = self.setoptions(settings_file)
+        rm_files = [os.path.join(settings.output_dir, f) for f in os.listdir(settings.output_dir) if f.endswith('.nc') or f.endswith('.tss')]
+        for f in rm_files:
+            os.unlink(f)
+
     def test_dates_steps_day(self):
         settings_a = self.setoptions(settings_file,
                                      opts_to_set=['repStateMaps', 'repEndMaps', 'repDischargeMaps',
                                                   'repSnowMaps', 'repLZMaps', 'repUZMaps'],
                                      opts_to_unset=['simulateLakes'])
-        out_a = settings_a.output_dir
         lisfloodexe(settings_a)
         settings_b = self.setoptions(settings_file,
                                      opts_to_set=['repStateMaps', 'repEndMaps', 'repDischargeMaps',
@@ -27,14 +38,24 @@ class TestStepsDates(TestSettings):
                                      opts_to_unset=['simulateLakes'],
                                      vars_to_set={'StepStart': 213, 'StepEnd': 215,
                                                   'PathOut': '$(PathRoot)/outputs/compare'})
-        out_b = settings_b.output_dir
+
         lisfloodexe(settings_b)
+
         assert settings_a.step_start_int == 213
         assert settings_a.step_end_int == 215
         assert settings_a.step_start == settings_a.step_start_dt.strftime('%d/%m/%Y %H:%M')
         assert settings_a.step_end == settings_a.step_end_dt.strftime('%d/%m/%Y %H:%M')
         assert settings_b.step_start_dt == datetime.datetime(2000, 7, 30, 6, 0)
         assert settings_b.step_end_dt == datetime.datetime(2000, 8, 1, 6, 0)
+
         maskinfo = MaskInfo.instance()
         comparator = NetCDFComparator(maskinfo.info.mask)
-        comparator.compare_dirs(out_a, out_b)
+        out_a = settings_a.output_dir
+        out_b = settings_b.output_dir
+        errors = comparator.compare_dirs(out_a, out_b)
+        assert not errors
+
+        rm_files = [os.path.join(out_a, f) for f in os.listdir(out_a) if f.endswith('.nc') or f.endswith('.tss')]
+        rm_files += [os.path.join(out_b, f) for f in os.listdir(out_b) if f.endswith('.nc') or f.endswith('.tss')]
+        for f in rm_files:
+            os.unlink(f)
