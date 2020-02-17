c47a6794b602fc49844d7ed065d254532eaa6a2d
Domenico Nappo <domenico.nappo@gmail.com>
2020-02-17 11:20:23 +0100

    adjusted Dockerfile #41

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,9 +3,9 @@
 set -e
 if [[ "$1" = 'usecases' ]]; then
     mkdir -p /usecases
-    echo "Copying maps to /input/Drina..."
-    cp -R /tests/data/TestCatchment1 /usecases/
-    chmod a+w /usecases/TestCatchment1/settings/*.xml
+    echo "Copying maps to /usecases/TestCatchment/..."
+    cp -R /tests/data/TestCatchment /usecases/
+    chmod a+w /usecases/TestCatchment/settings/*.xml
 else
     exec python /lisf1.py "$@"
 fi
