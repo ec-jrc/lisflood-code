b3c3ba38b35f57e3a927ca3106644f5be1503449
Carlo Russo <carlo.russo@email.it>
2022-11-14 13:59:09 +0100

    Updated rep state variables in meteo, gauges and sites. Rename ThetaTS in ThetaAvUpsTS for gauge state variables. Updated reported Unit Test and docs

--- tests/test_reported_tss.py
+++ tests/test_reported_tss.py
@@ -55,46 +55,51 @@ class TestReportedTSS():
     def test_rep_stateupgaugestss(self, mocker):
         self._reported_tss(self.settings_files['full'], opts_to_set=['repStateUpsGauges'],
                            tss_to_check=['dslrUps.tss', 'frostUps.tss', 'lzUps.tss',
-                                         'scovUps.tss', 'wDepthUps.tss'],
+                                         'scovUps.tss', 'wDepthUps.tss',
+                                         'th1aAvUps.tss', 'th1bAvUps.tss', 'th2AvUps.tss'], 
                            mocker=mocker)
 
     def test_rep_stateupgaugestss_not_called(self, mocker):
         self._not_reported_tss(self.settings_files['full'],
                                tss_to_check=['dslrUps.tss', 'frostUps.tss', 'lzUps.tss',
-                                             'scovUps.tss', 'wDepthUps.tss'], mocker=mocker)
+                                             'scovUps.tss', 'wDepthUps.tss',
+                                             'th1aAvUps.tss', 'th1bAvUps.tss', 'th2AvUps.tss'], 
+                               mocker=mocker)
 
     def test_rep_rateupgaugestss(self, mocker):
         self._reported_tss(self.settings_files['full'], opts_to_set=['repRateUpsGauges'],
                            tss_to_check=['percUZLZUps.tss', 'infUps.tss', 'qLzUps.tss', 'qUzUps.tss',
-                                         'dTopToSubUps.tss', 'prefFlowUps.tss', 'rainUps.tss',
-                                         'snowUps.tss', 'snowMeltUps.tss', 'surfaceRunoffUps.tss'],
+                                         'dTopToSubUps.tss', 'prefFlowUps.tss',
+                                         'snowMeltUps.tss', 'surfaceRunoffUps.tss'],
                            mocker=mocker)
 
     def test_rep_rateupgaugestss_not_called(self, mocker):
         self._not_reported_tss(self.settings_files['full'],
                                tss_to_check=['percUZLZUps.tss', 'infUps.tss', 'qLzUps.tss', 'qUzUps.tss',
-                                             'dTopToSubUps.tss', 'prefFlowUps.tss', 'rainUps.tss',
-                                             'snowUps.tss', 'snowMeltUps.tss', 'surfaceRunoffUps.tss'],
+                                             'dTopToSubUps.tss', 'prefFlowUps.tss', 
+                                             'snowMeltUps.tss', 'surfaceRunoffUps.tss'],
                                mocker=mocker)
 
     def test_rep_meteoupgaugestss(self, mocker):
         self._reported_tss(self.settings_files['full'], opts_to_set=['repMeteoUpsGauges'],
-                           tss_to_check=['etUps.tss', 'ewUps.tss', 'tAvgUps.tss'],
+                           tss_to_check=['etUps.tss', 'ewUps.tss', 'tAvgUps.tss', 'snowUps.tss', 'rainUps.tss'],
                            mocker=mocker)
 
     def test_rep_meteoupgaugestss_not_called(self, mocker):
         self._not_reported_tss(self.settings_files['full'],
-                               tss_to_check=['etUps.tss', 'ewUps.tss', 'tAvgUps.tss'],
+                               tss_to_check=['etUps.tss', 'ewUps.tss', 'tAvgUps.tss', 'snowUps.tss', 'rainUps.tss'],
                                mocker=mocker)
 
     def test_rep_statesitestss(self, mocker):
         self._reported_tss(self.settings_files['full'], opts_to_set=['repStateSites'],
-                           tss_to_check=['dslr.tss', 'frost.tss', 'lz.tss','scov.tss','wDepth.tss'],
+                           tss_to_check=['dslr.tss', 'frost.tss', 'lz.tss','scov.tss','wDepth.tss',
+                                         'th1a.tss', 'th1b.tss', 'th2.tss'],
                            mocker=mocker)
 
     def test_rep_statesitestss_not_called(self, mocker):
         self._not_reported_tss(self.settings_files['full'],
-                               tss_to_check=['dslr.tss', 'frost.tss', 'lz.tss','scov.tss','wDepth.tss'],
+                               tss_to_check=['dslr.tss', 'frost.tss', 'lz.tss','scov.tss','wDepth.tss',
+                                             'th1a.tss', 'th1b.tss', 'th2.tss'],
                                mocker=mocker)
 
     def test_rep_ratesitestss(self, mocker):
