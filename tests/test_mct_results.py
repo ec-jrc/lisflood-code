49e4ee2f55a4f32bf096690a91c2c35c97b1216f
Cinzia Mazzetti <mocm@ecmwf.int>
2024-07-03 17:12:07 +0000

    refactored the test for MCT results

--- tests/test_mct_results.py
+++ tests/test_mct_results.py
@@ -1,81 +1,94 @@
-"""
-
-Copyright 2019 European Union
-
-Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission  subsequent versions of the EUPL (the "Licence");
-
-You may not use this work except in compliance with the Licence.
-You may obtain a copy of the Licence at:
-
-https://joinup.ec.europa.eu/sites/default/files/inline-files/EUPL%20v1_2%20EN(1).txt
-
-Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the Licence for the specific language governing permissions and limitations under the Licence.
-
-"""
-from __future__ import absolute_import, print_function
-
+from __future__ import absolute_import
 import os
+import datetime
 import shutil
-
 import pytest
 
-from lisflood.global_modules.settings import LisSettings
+from lisfloodutilities.compare.nc import NetCDFComparator
+from lisfloodutilities.compare.pcr import TSSComparator
+
 from lisflood.main import lisfloodexe
+from lisflood.global_modules.settings import LisSettings
 
-from .test_utils import setoptions, mk_path_out, MCTTestCase
+from .test_utils import setoptions, mk_path_out
 
 
-@pytest.mark.slow
-class TestCatch(MCTTestCase):
+class TestTSSResults():
+
     case_dir = os.path.join(os.path.dirname(__file__), 'data', 'LF_MCT_UseCase')
-    modules_to_set = (
-        'MCTRouting',
-    )
-    settings_files = {
-        'cold': os.path.join(case_dir, 'settings/mct_cold.xml')
-    }
 
-    def run(self, dt_sec, dt_sec_chan, step_start, step_end):
-        output_dir = mk_path_out(os.path.join(self.case_dir, 'out/test_results{}'.format(dt_sec)))
-        opts_to_unset = (
-            "repStateSites", "repRateSites", "repStateUpsGauges", "repRateUpsGauges", "repMeteoUpsGauges",
-            "repsimulateLakes", "repStateMaps",
-            "repsimulateReservoirs", "repSnowMaps", "repPFMaps", "repLZMaps", "repUZMaps",
-            "repGwPercUZLZMaps", "repRWS", "repTotalWUse", "repWIndex",
-            "repSurfaceRunoffMaps", "repRainMaps", "repSnowMaps", "repSnowCoverMaps", "repSnowMeltMaps", 
-            "repLZMaps", "repUZMaps",
-            "repGwPercUZLZMaps", "repRWS", "repPFMaps", "repPFForestMaps"
-        )
-        settings = setoptions(self.settings_files['cold'],
-                              opts_to_set=('repDischargeTs', 'repDischargeMaps',
-                                            'repMBTs') + self.modules_to_set,
-                              opts_to_unset=opts_to_unset,
-                              vars_to_set={'StepStart': step_start,
-                                           'StepEnd': step_end,
-                                           'DtSec': dt_sec,
-                                           'DtSecChannel': dt_sec_chan,
-                                           'PathOut': output_dir})
+    def run(self, date_start, date_end, dtsec, dtsec_chan, type):
+        # generate lisflood results
+        out_path_ref = os.path.join(self.case_dir, 'reference', 'output_reference_'+type)
+        self.out_path_run = os.path.join(self.case_dir, 'out', 'output_'+type)
+        settings_file = os.path.join(self.case_dir, 'settings', 'mct_cold.xml')
+        settings = setoptions(settings_file,
+                              opts_to_set = ['MCTRouting'],
+                              opts_to_unset=['SplitRouting'],
+                              vars_to_set={'StepStart': date_start,
+                                           'StepEnd': date_end,
+                                           'CalendarDayStart': date_start,
+                                           'DtSec' : dtsec,
+                                           'DtSecChannel' : dtsec_chan,        # single routing step
+                                           'PathOut': self.out_path_run})
+
+        mk_path_out(self.out_path_run)
         lisfloodexe(settings)
 
-    def test_output_daily(self):
-        self.run('86400', '3600', '02/01/2016 06:00', '02/07/2016 06:00')
-        self.compare_reference('dis', check='map', step_length='86400')
-        self.compare_reference('dis', check='tss', step_length='86400')
-        self.compare_reference('chanq', check='tss', step_length='86400')
-        self.compare_reference('mbError', check='tss', step_length='86400')
+        # set precisioon for the test
+        atol = 0.0001
+        rtol = 0.001
+        comparator = TSSComparator(atol,rtol)
 
-    # def test_output_6h_6h(self):
-    #     self.run('21600', '21600','02/01/2016 06:00', '02/07/2016 06:00')
-    #     self.compare_reference('dis', check='map', step_length='21600')
-    #     self.compare_reference('dis', check='tss', step_length='21600')
-    #     self.compare_reference('chanq', check='tss', step_length='21600')
-    #     self.compare_reference('mbError', check='tss', step_length='21600')
-    #
-    # def test_output_6h_1h(self):
-    #     self.run('21600', '3600','02/01/2016 06:00', '02/07/2016 06:00')
-    #     self.compare_reference('dis', check='map', step_length='21600')
-    #     self.compare_reference('dis', check='tss', step_length='21600')
-    #     self.compare_reference('chanq', check='tss', step_length='21600')
-    #     self.compare_reference('mbError', check='tss', step_length='21600')
+        # compare results for average discharge output
+        reference =  os.path.join(out_path_ref, 'disX.tss')
+        output_tss =  os.path.join(self.out_path_run, 'disX.tss')
+
+        comparator.compare_files(reference, output_tss)
+
+        # compare results for instant discharge output
+        reference =  os.path.join(out_path_ref, 'chanqX.tss')
+        output_tss =  os.path.join(self.out_path_run, 'chanqX.tss')
+
+        comparator.compare_files(reference, output_tss)
+
+        # compare mass balance error
+        reference =  os.path.join(out_path_ref, 'mbError.tss')
+        output_tss =  os.path.join(self.out_path_run, 'mbError.tss')
+
+        comparator.compare_files(reference, output_tss)
+
+
+    def teardown_method(self):
+        print('Cleaning directories')
+        out_path = os.path.join(self.case_dir, self.out_path_run)
+        shutil.rmtree(out_path, ignore_errors=True)
+
+
+class TestMCTResults(TestTSSResults):
+
+    run_type = 'short'
+
+    def test_MCT_6h(self):
+        self.run("02/01/2016 06:00", "02/07/2016 06:00", 21600, 21600,'6h')
+
+    def test_MCT_6h_1h(self):
+        self.run("02/01/2016 06:00", "02/07/2016 06:00", 21600, 3600,'6h_1h')
+
+    def test_MCT_daily(self):
+         self.run("02/01/2016 06:00", "02/07/2016 06:00", 86400, 86400,'daily')
+
+    def test_MCT_daily_6h(self):
+         self.run("02/01/2016 06:00", "02/07/2016 06:00", 86400, 21600,'daily_6h')
+
+    def test_MCT_daily_1h(self):
+         self.run("02/01/2016 06:00", "02/07/2016 06:00", 86400, 3600, 'daily_1h')
+
+
+# @pytest.mark.slow
+# class TestInflowLong(TestInflow):
+#
+#     run_type = 'long'
+#
+#     def test_inflow_short(self):
+#         self.run("02/01/1986 00:00", "01/01/2018 00:00")
