1867eed062937933b6daf79b7572dd83da88b67d
Cinzia Mazzetti <mocm@ecmwf.int>
2025-01-30 11:38:36 +0000

    Restoring test_results and bugfix

--- tests/test_mct_warmstart_slow.py
+++ tests/test_mct_warmstart_slow.py
@@ -86,10 +86,11 @@ class TestWarmStartLong():
         self.path_out_reference = os.path.join(self.case_dir, 'out', 'longrun_reference{}'.format(dt_sec))
 
         if mct_case == 'mct':
-            opts_to_set = ['repStateMaps']
+            opts_to_set = ['repStateMaps','repDischargeMaps']
             opts_to_unset = ['repMBTs', 'simulateReservoirs', 'simulateLakes']
         elif mct_case == 'mct_all':
             opts_to_set = ['repStateMaps',
+                           'repDischargeMaps',
                            'wateruse',
                            'drainedIrrigation',
                            'riceIrrigation',
