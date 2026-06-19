cf1ebd89744ace12513bc51bbe2c2ecd2c5e93cd
doc78 <crl.russo@hotmail.com>
2026-06-19 14:25:43 +0000

    Added author list for Zenodo pubblication. Updated version number for release 5.0.0

--- src/lisflood/__init__.py
+++ src/lisflood/__init__.py
@@ -9,9 +9,9 @@ with open(version_file, 'r') as f:
     version = f.read().strip()
 
 __version__ = version
-__authors__ = "Ad de Roo, Emiliano Gelati, Peter Burek, Johan van der Knijff, Niko Wanders"
-__date__ = "27/02/2025"
-__copyright__ = "Copyright 2019-2025, European Commission - Joint Research Centre"
-__maintainer__ = "Stefania Grimaldi, Cinzia Mazzetti, Jesus Casado Rodriguez, Carlo Russo, Damien Decremer, Corentin Carton De Wiart"
+__authors__ = "Ad de Roo, Emiliano Gelati, Peter Burek, Johan van der Knijff"
+__date__ = "18/06/2026"
+__copyright__ = "Copyright 2019-2026, European Commission - Joint Research Centre"
+__maintainer__ = "Stefania Grimaldi, Timo Schaffhauser, Carlo Russo, Cinzia Mazzetti, Corentin Carton De Wiart"
 __status__ = "Operation"
 __institution__ = "European Commission DG Joint Research Centre (JRC) - E1, D2 Units"
