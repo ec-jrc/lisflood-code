5882f241a02c528c9117e7b85cfec7ab8ffdfe68
Timo Schaffhauser <Timo.SCHAFFHAUSER@ec.europa.eu>
2026-07-28 12:41:00 +0200

    Update

--- src/lisflood/global_modules/settings.py
+++ src/lisflood/global_modules/settings.py
@@ -841,6 +841,15 @@ class LisfloodRunInfo(Warning):
         msg += "\t[X] The simulation output as specified in the settings file can be found in {}\n".format(out_dir)
         msg += "\t[X] Activated modules: {}\n".format(activated_options)
         msg += "\t[X] Report options: {}\n".format(activated_rep)
+        msg += "\t[X] Activated modules: {}\n".format(activated_options)
+        msg += "\t[X] Report options: {}\n".format(activated_rep)
+        # Packing and aggregation info
+        if binding.get('OutputPacking', 'False') == 'True':
+            msg += "\t[X] Output Packing: int16 scale/offset enabled\n"
+        for agg_key in ['OutputMonthlyMean', 'OutputMonthlySum', 'OutputYearlyMean', 'OutputYearlySum']:
+            agg_val = binding.get(agg_key, '').strip()
+            if agg_val:
+                msg += "\t[X] {}: {}\n".format(agg_key, agg_val)
         self._msg = '{}{}'.format(header, msg)
 
     def __str__(self):
